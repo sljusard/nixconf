81dbe06374ee0c46213bb1de6562c7b394cffc87
Denis Sliusar <sljusarde@gmail.com>
2026-05-04 23:32:15 +0300

    Changed default locale to C

--- modules/hosts/main/configuration.nix
+++ modules/hosts/main/configuration.nix
@@ -77,7 +77,7 @@
 
     time.timeZone = "Europe/Moscow";
 
-    i18n.defaultLocale = "ru_RU.UTF-8";
+    i18n.defaultLocale = "C.UTF-8";
     i18n.extraLocaleSettings = {
       LC_ADDRESS = "ru_RU.UTF-8";
       LC_IDENTIFICATION = "ru_RU.UTF-8";
