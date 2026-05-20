fa9835d411deef2186326d06a4712ea22d5c0964
Denis Sliusar <sljusarde@gmail.com>
2026-05-21 01:28:42 +0300

    Fixed nvim-tree config duplication

--- modules/features/wrapped/neovim/init.lua
+++ modules/features/wrapped/neovim/init.lua
@@ -1,10 +1,10 @@
 require("options")
-vim.pack.add({
-  { src = 'https://github.com/nvim-tree/nvim-web-devicons' }, -- optional
-  { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
-})
+-- vim.pack.add({
+--   { src = 'https://github.com/nvim-tree/nvim-web-devicons' }, -- optional
+--  { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
+-- })
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-require("nvim-tree").setup()
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+require("plugins.nvim-tree")
 -- require('lz.n').load('plugins')
