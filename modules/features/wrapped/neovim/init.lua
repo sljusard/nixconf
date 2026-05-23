323a92daaaaf6441fdda1b7a77e1479f689a80b2
Denis Sliusar <sljusarde@gmail.com>
2026-05-23 19:09:51 +0300

    Minor nvim tweaks

--- modules/features/wrapped/neovim/init.lua
+++ modules/features/wrapped/neovim/init.lua
@@ -1,10 +1,6 @@
 require("options")
--- vim.pack.add({
---   { src = 'https://github.com/nvim-tree/nvim-web-devicons' }, -- optional
---  { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
--- })
-
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
 require("plugins.nvim-tree")
--- require('lz.n').load('plugins')
+
+vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } }
+
+vim.cmd.colorscheme "catppuccin-mocha"
