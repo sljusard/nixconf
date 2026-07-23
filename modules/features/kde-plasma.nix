07bef6f84d47f32334fb4aae6dbafe5000944e36
Denis Slyusar <sljusarde@gmail.com>
2026-07-24 02:33:31 +0300

    feat: remove unnecessary KDE packages

--- modules/features/kde-plasma.nix
+++ modules/features/kde-plasma.nix
@@ -1,16 +1,30 @@
 { self, inputs, ... }: {
 
   flake.nixosModules.kde-plasma = { pkgs, lib, ... }: {
-
-    services.displayManager.sddm.enable = true;
     services.desktopManager.plasma6.enable = true;
-
-    environment.plasma6.excludePackages = with pkgs; [
-      kdePackages.elisa
-      kdePackages.kate
-      kdePackages.konsole
-      kdePackages.kwalletmanager
-      kdePackages.qrca
+    environment.plasma6.excludePackages = with pkgs.kdePackages; [
+      ark
+      dolphin
+      elisa
+      gwenview
+      kate
+      kcalc
+      khelpcenter
+      konsole
+      okular
+      spectacle
+      discover
+      kdeconnect-kde
+      kdegraphics-thumbnailers
+      kdenetwork-filesharing
+      ffmpegthumbs
+      plasma-browser-integration
+      plasma-vault
+      print-manager
+      kwrited
+      krdp
+      kinfocenter
+      plasma-systemmonitor
     ];
 
   }; 
