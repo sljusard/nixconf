6da7b64e0c52d0189b80c2dcc7d605793e9e9032
Denis Slyusar <sljusarde@gmail.com>
2026-07-25 04:27:42 +0500

    feat(ecoserver): fish as interactive shell

--- modules/hosts/server/configuration.nix
+++ modules/hosts/server/configuration.nix
@@ -91,6 +91,16 @@
 
     programs.fish.enable = true;
     programs.fish.package = selfpkgs.myEnvironment;
+    programs.bash = {
+      interactiveShellInit = ''
+        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        then
+          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+          exec ${selfpkgs.myEnvironment}/bin/fish $LOGIN_OPTION
+        fi
+      '';
+    };
+
 
     # Reverse Proxy settings
     # -----------------------------------------------------
